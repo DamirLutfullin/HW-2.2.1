ef526b0741884a9057d9867a6c89eb81bcf8e1f2
Damir Lutfullin <dmr.lutfullin@gmail.com>
2020-02-21 14:07:30 +0500

    Initial Commit

---
+++ HW 2.2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW 2.2.1
+//
+//  Created by Дамир Лутфуллин on 21.02.2020.
+//  Copyright © 2020 Damir Lutfullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
